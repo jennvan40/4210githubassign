67c129d76ce6317563ce4c6f5b6856ecacca3383
jennvan40 <126990753+jennvan40@users.noreply.github.com>
2024-02-16 22:24:56 -0700

    adding nav links

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.markdown("# Welcome to my Website!")
 st.sidebar.markdown("# Main Page")
 
-st.write("Click on a page to see racer stats")
+st.write("Click on a page to see Kart or racer stats")
+
+link = '[To my GitHub Pages Site](https://jennvan40.github.io/4210githubassign/)'
+st.markdown(link, unsafe_allow_html=True)
